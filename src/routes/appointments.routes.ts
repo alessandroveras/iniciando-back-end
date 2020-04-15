06a85a5a886cddacc81eff8f0f9a6c0446f69c92
Alessandro Veras <alessandro.veras@gmail.com>
2020-04-15 17:56:26 -0300

    final section module 3

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,10 +1,11 @@
+// libs
 import Router from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-
+// services
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
-
+// middlewares
 import enforceAuthentication from '../middlewares/enforceAuthentication';
 
 const appointmentsRouter = Router();
@@ -19,22 +20,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      provider_id,
-      date: parsedDate,
-    });
+  const appointment = await createAppointment.execute({
+    provider_id,
+    date: parsedDate,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,27 +1,45 @@
+// libs
 import Router from 'express';
-
+import multer from 'multer';
+// services
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+// middlewares
+import enforceAuthentication from '../middlewares/enforceAuthentication';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.use('/avatar', enforceAuthentication, upload.single('avatar'));
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(user);
+});
+
+usersRouter.patch('/avatar', async (request, response) => {
+  const updateUserAvatar = new UpdateUserAvatarService();
+
+  const user = await updateUserAvatar.execute({
+    user_id: request.user.id,
+    avatarFilename: request.file.filename,
+  });
+
+  delete user.password;
+
+  return response.json(user);
 });
 
 export default usersRouter;
