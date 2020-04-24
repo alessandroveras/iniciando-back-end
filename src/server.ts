faeacb87028e2c7a790c875fcabbb65bdce8e6f1
Alessandro Veras <alessandro.veras@gmail.com>
2020-04-24 03:12:53 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 // libs
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 // app
 import routes from './routes';
@@ -10,6 +11,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
