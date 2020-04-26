ebef98aeab9f57ed9d83dc0844fd00b48794b1be
Alessandro Veras <alessandro.veras@gmail.com>
2020-04-26 00:15:59 -0300

    ajustes redeploy

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+
 // app
 import routes from './routes';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
-
 import './database';
 
 const app = express();
